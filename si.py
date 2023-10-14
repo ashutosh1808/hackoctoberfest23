f0a015778bab61437e544302b4c5943ae8053f02
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2023-10-14 23:30:05 +0530

    Update si.py

--- si.py
+++ si.py
@@ -5,7 +5,7 @@ def simple_interest(p,t,r):
     print('The rate of interest is',r)
      
     si = (p * t * r)/100
-     
+    si=round(si,2)
     print('The Simple Interest is', si)
     return si
 simple_interest(8, 6, 8)
